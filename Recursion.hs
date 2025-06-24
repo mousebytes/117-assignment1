99b94a241b158b5488faa69512f57f0c1000b193
mousebytes <alfonso.morales625625@gmail.com>
2025-06-23 20:47:04 -0700

    first push

---
+++ Recursion.hs
@@ -0,0 +1,169 @@
+---- Part 1: Basic structural recursion ----------------
+
+-- 1. Merge sort
+
+-- Deal a list into two (almost) equal-sized lists by alternating elements
+-- For example, deal [1,2,3,4,5,6,7] = ([1,3,5,7], [2,4,6])
+-- and          deal   [2,3,4,5,6,7] = ([2,4,6], [3,5,7])
+-- Hint: notice what's happening between the answers to deal [2..7] and
+-- deal (1:[2..7]) above to get an idea of how to approach the recursion
+deal :: [a] -> ([a], [a])
+deal [] = ([], [])
+deal (x : xs) =
+  let (ys, zs) = deal xs
+   in (x : zs, ys)
+
+-- Now implement merge and mergesort (ms), and test with some
+-- scrambled lists to gain confidence that your code is correct
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x : xs) (y : ys)
+  -- we keep the smallest value then call merge on the
+  -- rest of the list until we end up with something like
+  -- 1:2:3:4:5[] from [1,2,3] [4,5]
+  | x <= y = x : merge xs (y : ys)
+  | x > y = y : merge (x : xs) ys
+
+ms :: (Ord a) => [a] -> [a]
+ms [] = []
+ms [x] = [x]
+-- general case: deal, recursive call, merge
+ms xs =
+  let (ys, zs) = deal xs -- here we capture the two output arrays
+   in merge (ms ys) (ms zs) -- call the ms function on the newly split arrays then merge the final values
+
+-- 2. A backward list data structure
+
+-- Back Lists: Lists where elements are added to the back ("snoc" == rev "cons")
+-- For example, the list [1,2,3] is represented as Snoc (Snoc (Snoc Nil 1) 2) 3
+data BList a = Nil | Snoc (BList a) a deriving (Show, Eq)
+
+-- Add an element to the beginning of a BList, like (:) does
+cons :: a -> BList a -> BList a
+cons n Nil = Snoc Nil n -- inner list means add this snoc
+cons n (Snoc bl n2) = Snoc (cons n bl) n2 -- call until you reach inner list
+
+-- Convert a usual list into a BList (hint: use cons in the recursive case)
+toBList :: [a] -> BList a
+toBList [] = Nil
+toBList (x : xs) = cons x $ toBList xs
+
+-- Add an element to the end of an ordinary list, like Snoc does
+snoc :: [a] -> a -> [a]
+snoc = undefined
+
+-- Convert a BList into an ordinary list (hint: use snoc in the recursive case)
+fromBList :: BList a -> [a]
+fromBList = undefined
+
+-- 3. A binary tree data structure
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Eq)
+
+-- Count number of Empty's in the tree
+num_empties :: Tree a -> Int
+num_empties = undefined
+
+-- Count number of Node's in the tree
+num_nodes :: Tree a -> Int
+num_nodes = undefined
+
+-- Insert a new node in the leftmost spot in the tree
+insert_left :: a -> Tree a -> Tree a
+insert_left = undefined
+
+-- Insert a new node in the rightmost spot in the tree
+insert_right :: a -> Tree a -> Tree a
+insert_right = undefined
+
+-- Add up all the node values in a tree of numbers
+sum_nodes :: (Num a) => Tree a -> a
+sum_nodes = undefined
+
+-- Produce a list of the node values in the tree via an inorder traversal
+-- Feel free to use concatenation (++)
+inorder :: Tree a -> [a]
+inorder = undefined
+
+---- Part 2: Iteration and Accumulators ----------------
+
+-- Both toBList and fromBList from Part 1 Problem 2 are O(n^2) operations.
+-- Reimplement them using iterative helper functions (locally defined using
+-- a 'where' clause) with accumulators to make them O(n)
+toBList' :: [a] -> BList a
+toBList' = undefined
+
+fromBList' :: BList a -> [a]
+fromBList' = undefined
+
+-- Even tree functions that do multiple recursive calls can be rewritten
+-- iteratively using lists of trees and an accumulator. For example,
+sum_nodes' :: (Num a) => Tree a -> a
+sum_nodes' t = sum_nodes_it [t] 0
+  where
+    sum_nodes_it :: (Num a) => [Tree a] -> a -> a
+    sum_nodes_it [] a = a
+    sum_nodes_it (Empty : ts) a = sum_nodes_it ts a
+    sum_nodes_it (Node n t1 t2 : ts) a = sum_nodes_it (t1 : t2 : ts) (n + a)
+
+-- Use the same technique to convert num_empties, num_nodes, and sum_nodes2
+-- into iterative functions with accumulators
+
+num_empties' :: Tree a -> Int
+num_empties' = undefined
+
+num_nodes' :: Tree a -> Int
+num_nodes' = undefined
+
+sum_nodes2' :: (Num a) => Tree a -> a
+sum_nodes2' = undefined
+
+---- Part 3: Higher-order functions ----------------
+
+-- The functions map, all, any, filter, dropWhile, takeWhile, and break
+-- from the Prelude are all higher-order functions. Reimplement them here
+-- as list recursions. break should process each element of the list at
+-- most once. All functions should produce the same output as the originals.
+
+my_map :: (a -> b) -> [a] -> [b]
+my_map = undefined
+
+my_all :: (a -> Bool) -> [a] -> Bool
+my_all = undefined
+
+my_any :: (a -> Bool) -> [a] -> Bool
+my_any = undefined
+
+my_filter :: (a -> Bool) -> [a] -> [a]
+my_filter = undefined
+
+my_dropWhile :: (a -> Bool) -> [a] -> [a]
+my_dropWhile = undefined
+
+my_takeWhile :: (a -> Bool) -> [a] -> [a]
+my_takeWhile = undefined
+
+my_break :: (a -> Bool) -> [a] -> ([a], [a])
+my_break = undefined
+
+-- Implement the Prelude functions and, or, concat using foldr
+
+my_and :: [Bool] -> Bool
+my_and = undefined
+
+my_or :: [Bool] -> Bool
+my_or = undefined
+
+my_concat :: [[a]] -> [a]
+my_concat = undefined
+
+-- Implement the Prelude functions sum, product, reverse using foldl
+
+my_sum :: (Num a) => [a] -> a
+my_sum = undefined
+
+my_product :: (Num a) => [a] -> a
+my_product = undefined
+
+my_reverse :: [a] -> [a]
+my_reverse = undefined
